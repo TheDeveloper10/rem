2b85064efd74f2d2c7e754ea59fa44b029e6ab3d
TheDeveloper10 <th3r3ap3r@abv.bg>
2022-08-31 19:17:18 +0300

    Added more unit tests

--- router_test.go
+++ router_test.go
@@ -16,24 +16,19 @@ type routerTest struct {
 }
 
 func TestRouter(t *testing.T) {
-	router := CreateDefaultRouter()
-
-	router.
-	NewBasicRoute("/basic-test").
-	GetRoute(func(res IResponse, req IRequest) bool {
-		res.Status(http.StatusOK)
-		return true
-	}).
-	PostRoute(func(res IResponse, req IRequest) bool {
-		res.Status(http.StatusCreated)
-		return true
-	})
+	router := createRouter()
 
 	tests := []routerTest{
 		{ 0, "/basic-test", http.MethodGet, nil, http.StatusOK },
 		{ 1, "/basic-test", http.MethodPost, nil, http.StatusCreated },
 		{ 2, "/basic-test", http.MethodDelete, nil, http.StatusMethodNotAllowed },
 		{ 3, "/unknown-path", http.MethodPatch, nil, http.StatusNotFound },
+		{ 4, "/query-single-int-param-test", http.MethodGet, nil, http.StatusBadRequest },
+		{ 5, "/query-single-int-param-test?v", http.MethodGet, nil, http.StatusBadRequest },
+		{ 6, "/query-single-int-param-test?v=", http.MethodGet, nil, http.StatusBadRequest },
+		{ 7, "/query-single-int-param-test?v=1", http.MethodGet, nil, http.StatusOK },
+		{ 8, "/query-single-int-param-test?v=2", http.MethodGet, nil, http.StatusAccepted },
+		{ 9, "/query-single-int-param-test?v=3", http.MethodGet, nil, http.StatusForbidden },
 	}
 
 	for _, test := range tests {
@@ -50,4 +45,46 @@ func TestRouter(t *testing.T) {
 			t.Errorf("TestId: %v\tExpected Status: %v\tReceived Status: %v", test.id, test.expectedStatus, res.StatusCode)
 		}
 	}
+}
+
+func createRouter() *Router {
+	router := CreateDefaultRouter()
+
+	router.
+		NewBasicRoute("/basic-test").
+		GetRoute(func(res IResponse, req IRequest) bool {
+			res.Status(http.StatusOK)
+			return true
+		}).
+		PostRoute(func(res IResponse, req IRequest) bool {
+			res.Status(http.StatusCreated)
+			return true
+		})
+
+	router.
+		NewBasicRoute("/query-single-int-param-test").
+		GetRoute(func(res IResponse, req IRequest) bool {
+			v := req.GetQueryParameters().Get("v")
+
+			if v == "" {
+				res.Status(http.StatusBadRequest)
+			} else if v == "1" {
+				res.Status(http.StatusOK)
+			} else if v == "2" {
+				res.Status(http.StatusAccepted)
+			} else {
+				res.Status(http.StatusForbidden)
+			}
+
+			return true
+		})
+
+	//router.
+	//	NewBasicRoute("/query-multi-int-param-test").
+	//	PostRoute(func(res IResponse, req IRequest) bool {
+	//
+	//		return true
+	//	})
+
+	return router
 }
